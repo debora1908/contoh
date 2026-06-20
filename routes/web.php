32d5dc42714cd6731acd8915b811df04ba08f95a
Debora1908 <corysimarmata19@gmail.com>
2026-06-21 02:35:35 +0700

    style: design elegant hotel landing page welcome view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\KamarController;
 use Illuminate\Support\Facades\Route;
-
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/kamar', [KamarController::class, 'index'])->name('kamar.index');
 Route::post('/kamar', [KamarController::class, 'store'])->name('kamar.store');
