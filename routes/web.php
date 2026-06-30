b4cbc6108a8fa663f2a24f1c54b4e290ef648c1f
Debora1908 <corysimarmata19@gmail.com>
2026-06-30 21:16:23 +0700

    perbaikan halaman pengguna

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -6,8 +5,16 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ReservasiController;
 use App\Http\Controllers\KamarController;
+
 use App\Http\Controllers\Admin\KamarController as AdminKamarController;
 use App\Http\Controllers\Admin\ReservasiController as AdminReservasiController;
+use App\Http\Controllers\Admin\PenggunaController;
+
+/*
+|--------------------------------------------------------------------------
+| HALAMAN UTAMA
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,104 +32,88 @@ Route::get('/wellness', function () {
     return view('wellness');
 })->name('wellness.index');
 
+/*
+|--------------------------------------------------------------------------
+| KAMAR
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/kamar', [KamarController::class, 'index'])
-    ->name('kamar.index');
+Route::get('/kamar', [KamarController::class, 'index'])->name('kamar.index');
+Route::post('/kamar', [KamarController::class, 'store'])->name('kamar.store');
 
-Route::post('/kamar', [KamarController::class, 'store'])
-    ->name('kamar.store');
+/*
+|--------------------------------------------------------------------------
+| RESERVASI TAMU
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/reservasi', [ReservasiController::class, 'index'])
-    ->name('reservasi.index');
+Route::get('/reservasi', [ReservasiController::class, 'index'])->name('reservasi.index');
+Route::post('/reservasi', [ReservasiController::class, 'store'])->name('reservasi.store');
 
-Route::post('/reservasi', [ReservasiController::class, 'store'])
-    ->name('reservasi.store');
+/*
+|--------------------------------------------------------------------------
+| LOGIN
+|--------------------------------------------------------------------------
+*/
 
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.proses');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/login', [AuthController::class, 'showLogin'])
-    ->name('login');
+/*
+|--------------------------------------------------------------------------
+| BOOKING
+|--------------------------------------------------------------------------
+*/
 
-Route::post('/login', [AuthController::class, 'login'])
-    ->name('login.proses');
+Route::post('/booking/store', [BookingController::class, 'store'])->name('booking.store');
+Route::get('/booking/pembayaran/{id}', [BookingController::class, 'pembayaran'])->name('booking.pembayaran');
+Route::post('/booking/konfirmasi/{id}', [BookingController::class, 'konfirmasi'])->name('booking.konfirmasi');
+Route::get('/booking/{id}/edit', [BookingController::class, 'edit'])->name('booking.edit');
+Route::put('/booking/{id}', [BookingController::class, 'update'])->name('booking.update');
+Route::delete('/booking/{id}', [BookingController::class, 'destroy'])->name('booking.destroy');
 
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->name('logout');
-
-
-
-Route::post('/booking/store', [BookingController::class, 'store'])
-    ->name('booking.store');
-
-Route::get('/booking/pembayaran/{id}', [BookingController::class, 'pembayaran'])
-    ->name('booking.pembayaran');
-
-Route::post('/booking/konfirmasi/{id}', [BookingController::class, 'konfirmasi'])
-    ->name('booking.konfirmasi');
-
-Route::get('/booking/{id}/edit', [BookingController::class, 'edit'])
-    ->name('booking.edit');
-
-Route::put('/booking/{id}', [BookingController::class, 'update'])
-    ->name('booking.update');
-
-Route::delete('/booking/{id}', [BookingController::class, 'destroy'])
-    ->name('booking.destroy');
-
-
-
-Route::get('/admin/dashboard', [BookingController::class, 'index'])
-    ->name('admin.dashboard');
-
+/*
+|--------------------------------------------------------------------------
+| ADMIN
+|--------------------------------------------------------------------------
+*/
 
 Route::prefix('admin')->name('admin.')->group(function () {
 
-   
-    Route::get('/manajemen', [AdminKamarController::class, 'index'])
-        ->name('manajemen.index');
+    /*
+    | Dashboard
+    */
+    Route::get('/dashboard', [BookingController::class, 'index'])->name('dashboard');
 
-    Route::post('/manajemen', [AdminKamarController::class, 'store'])
-        ->name('manajemen.store');
+    /*
+    | Manajemen Kamar
+    */
+    Route::get('/manajemen', [AdminKamarController::class, 'index'])->name('manajemen.index');
+    Route::post('/manajemen', [AdminKamarController::class, 'store'])->name('manajemen.store');
+    Route::put('/manajemen/{id}', [AdminKamarController::class, 'update'])->name('manajemen.update');
+    Route::delete('/manajemen/{id}', [AdminKamarController::class, 'destroy'])->name('manajemen.destroy');
 
-    Route::put('/manajemen/{id}', [AdminKamarController::class, 'update'])
-        ->name('manajemen.update');
+    /*
+    | Reservasi Admin
+    */
+    Route::get('/reservasi', [AdminReservasiController::class, 'index'])->name('reservasi.index');
+    Route::get('/reservasi/{id}', [AdminReservasiController::class, 'detail'])->name('reservasi.detail');
+    Route::put('/reservasi/{id}', [AdminReservasiController::class, 'update'])->name('reservasi.update');
+    Route::delete('/reservasi/{id}', [AdminReservasiController::class, 'destroy'])->name('reservasi.destroy');
+    Route::put('/reservasi/checkin/{id}', [AdminReservasiController::class, 'checkIn'])->name('reservasi.checkin');
+    Route::put('/reservasi/checkout/{id}', [AdminReservasiController::class, 'checkOut'])->name('reservasi.checkout');
+    Route::get('/reservasi/cetak/{id}', [AdminReservasiController::class, 'cetak'])->name('reservasi.cetak');
 
-    Route::delete('/manajemen/{id}', [AdminKamarController::class, 'destroy'])
-        ->name('manajemen.destroy');
-
-
-    Route::get('/reservasi', [BookingController::class, 'reservasi'])
-        ->name('reservasi.index');
-
-    Route::get('/reservasi/{id}', [BookingController::class, 'detail'])
-        ->name('reservasi.detail');
-
-    Route::put('/reservasi/{id}', [BookingController::class, 'updateReservasi'])
-        ->name('reservasi.update');
-
-    Route::delete('/reservasi/{id}', [BookingController::class, 'destroyReservasi'])
-        ->name('reservasi.destroy');
-
-    Route::put('/reservasi/checkin/{id}', [BookingController::class, 'checkIn'])
-        ->name('reservasi.checkin');
-
-    Route::put('/reservasi/checkout/{id}', [BookingController::class, 'checkOut'])
-        ->name('reservasi.checkout');
-Route::get('/reservasi/cetak/{id}', [AdminReservasiController::class, 'cetak'])
-    ->name('reservasi.cetak');
+    /*
+    | Pengguna
+    */
+    Route::get('/pengguna', [PenggunaController::class, 'index'])->name('pengguna.index');
+    Route::post('/pengguna', [PenggunaController::class, 'store'])->name('pengguna.store');
+    Route::get('/pengguna/{id}', [PenggunaController::class, 'show'])->name('pengguna.show');
+    Route::put('/pengguna/{id}', [PenggunaController::class, 'update'])->name('pengguna.update');
+    Route::delete('/pengguna/{id}', [PenggunaController::class, 'destroy'])->name('pengguna.destroy');
+    Route::put('/pengguna/reset/{id}', [PenggunaController::class, 'resetPassword'])->name('pengguna.reset');
+    
 
 });
-Route::get('/pengguna', [App\Http\Controllers\Admin\PenggunaController::class, 'index'])->name('pengguna.index');
-Route::post('/pengguna', [App\Http\Controllers\Admin\PenggunaController::class, 'store'])->name('pengguna.store');
-Route::put('/pengguna/{id}', [App\Http\Controllers\Admin\PenggunaController::class, 'update'])->name('pengguna.update');
-Route::delete('/pengguna/{id}', [App\Http\Controllers\Admin\PenggunaController::class, 'destroy'])->name('pengguna.destroy');
-Route::get('/pengguna', [PenggunaController::class,'index'])->name('pengguna.index');
-
-Route::post('/pengguna', [PenggunaController::class,'store'])->name('pengguna.store');
-
-Route::get('/pengguna/{id}', [PenggunaController::class,'show'])->name('pengguna.show');
-
-Route::put('/pengguna/{id}', [PenggunaController::class,'update'])->name('pengguna.update');
-
-Route::delete('/pengguna/{id}', [PenggunaController::class,'destroy'])->name('pengguna.destroy');
-
-Route::put('/pengguna/reset/{id}', [PenggunaController::class,'resetPassword'])->name('pengguna.reset');
